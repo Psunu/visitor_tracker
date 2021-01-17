a7b461007d16871e89d1b3d765b78c5149e35e6f
Psunu <psw9255439@naver.com>
2021-01-17 21:53:02 +0900

    Add backend projects

---
+++ backend/visitor_tracker/src/main/java/com/example/visitor_tracker/VisitorTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.visitor_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/visitor_tracker/src/test/java/com/example/visitor_tracker/VisitorTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visitor_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
